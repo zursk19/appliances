623a121877026d7f5eb288eddd2ee5b226351397
Dawid Zurawski <david_zurawski@msn.com>
2019-08-23 18:18:57 +0200

    initialization

---
+++ src/main/java/pl/sellions/appliance/ApplianceApplication.java
@@ -0,0 +1,13 @@
+package pl.sellions.appliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceApplication.class, args);
+	}
+
+}
